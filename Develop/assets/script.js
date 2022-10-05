83de75837abc9dd4406df2695df1affe4a514ae5
Vegascruz <cruzv@students.westerntc.edu>
2022-10-04 20:34:39 -0500

    Website is done. Just have to adjust for different screen sizes. Will finish in the morning

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -1,21 +1,59 @@
-var todaysDate = moment().format("dddd, MMMM Do");
-//console.log(todaysDate);
-$('#currentDay').text(todaysDate);
+$(document).ready(function(){
 
-var $row = $('.row');
+    var todaysDate = moment().format("dddd, MMMM Do");
+    //console.log(todaysDate);
+    $('#currentDay').text(todaysDate);
 
-$('.hour').each(function(){
-    var time = moment(this.innerText, "HH:mm a");;
-    var currentTime = moment();
+    $('.hour').each(function(){
+        var time = moment(this.innerText, "HH:mm a");;
+        var currentTime = moment();
 
-    console.log(currentTime.hour());
-    //Determines if time slot is before or after
-    if(time.hour() < currentTime.hour()){
-        //console.log(time.toString());
-        $(this).parent().siblings('textarea').removeClass().addClass('past');
-    }else if(time.hour() > currentTime.hour()){
-        $(this).parent().siblings('textarea').removeClass().addClass('future');
-    }else{
-        $(this).parent().siblings('textarea').removeClass().addClass('present');
-    }
+        //Determines if time slot is before or after
+        if(time.hour() < currentTime.hour()){
+            //console.log(time.toString());
+            $(this).parent().siblings('textarea').removeClass().addClass('past');
+        }else if(time.hour() > currentTime.hour()){
+            $(this).parent().siblings('textarea').removeClass().addClass('future');
+        }else{
+            $(this).parent().siblings('textarea').removeClass().addClass('present');
+        }
+    });
+
+    
+    $('.saveBtn').each(function(){
+        var id = $(this).attr('id');
+        var event = localStorage.getItem(id);
+
+        $(this).siblings('textarea').val(event);
+    });
+
+    $('.saveBtn').on('click', function(){
+        var id = $(this).attr('id');
+        var event = $(this).siblings('textarea').val();
+        
+        if(event != ""){
+            localStorage.setItem(id, event);
+            $('#confirmation').removeClass('hide');
+            $('#confirmationClear').addClass('hide');
+        }
+
+    });
+
+    $('.clearBtn').on('click',function(){
+        localStorage.clear();
+        $('textarea').each(function(){
+            $(this).val(""); 
+        });
+
+        $('#confirmationClear').removeClass('hide');
+        $('#confirmation').addClass('hide');
+    });
+
+    $('textarea').on('click', function() {
+        $('#confirmationClear').addClass('hide');
+        $('#confirmation').addClass('hide');
+    });
+    //var events = $('#description').val();
+    //console.log(events);
+
 });
